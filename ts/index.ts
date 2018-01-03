32fa0480499e5958ce22cf092415105bbfb7bd4c
Andrew Faulkner <andfaulkner@gmail.com>
2018-01-03 10:38:51 -0500

    Added Kotlin lib; minor attempted fix in TS file

--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,5 @@
 const kotlin = require('kotlin') as {collections: {HashMap: any}};
-import * as javelinWrapperImport from '../build/js/index';
+// import * as javelinWrapperImport from '../build/js/index';
 
-import srcJsOutput from '../src-js/build/classes/main/src-js.js';
+import '../src-js/build/classes/main/src-js';
 
