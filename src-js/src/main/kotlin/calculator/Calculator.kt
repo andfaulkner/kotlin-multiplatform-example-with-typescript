7eac703cf4494621fbfdfd932f37de2c69fb90a5
Andrew Faulkner <andfaulkner@gmail.com>
2018-01-03 10:34:11 -0500

    Fixed modules; JSName export of root

--- src-js/src/main/kotlin/calculator/Calculator.kt
+++ src-js/src/main/kotlin/calculator/Calculator.kt
@@ -3,8 +3,11 @@
  */
 package calculator
 
+import kotlin.js.JsName
+
 actual fun log(msg: String) = console.log(msg)
 
+@JsName("root")
 fun main(args: Array<String>) {
     Calculator.runExperiments()
 }
