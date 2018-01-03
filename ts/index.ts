c847889916ecc4043ba7c86ce84927ad0dffe073
Andrew Faulkner <andfaulkner@gmail.com>
2018-01-03 10:36:41 -0500

    Fixed import syntax (partially) in TS root

--- ts/index.ts
+++ ts/index.ts
@@ -1,2 +1,5 @@
+const kotlin = require('kotlin') as {collections: {HashMap: any}};
+import * as javelinWrapperImport from '../build/js/index';
+
 import srcJsOutput from '../src-js/build/classes/main/src-js.js';
 
