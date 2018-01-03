04a2d0d67da1c183f165a5f731d262c20552bb62
Andrew Faulkner <andfaulkner@gmail.com>
2018-01-03 11:35:18 -0500

    Docs in JS Kotlin; Properly logs output in TS code

--- src-js/src/main/kotlin/calculator/Calculator.kt
+++ src-js/src/main/kotlin/calculator/Calculator.kt
@@ -5,9 +5,17 @@ package calculator
 
 import kotlin.js.JsName
 
+@JsName("log")
 actual fun log(msg: String) = console.log(msg)
 
 @JsName("root")
-fun main(args: Array<String>) {
+fun root(args: Array<String>) {
     Calculator.runExperiments()
 }
+
+/**
+ * We don't actually need a main routine for JS-KT. However, if we do write
+ * one, it will execute immediately on loading the module.
+ */
+// @JsName("main")
+// fun main(args: Array<String>) {}
--- ts/index.ts
+++ ts/index.ts
@@ -2,4 +2,5 @@ const kotlin = require('kotlin');
 
 import * as srcJsOutput from '../src-js/build/js/index';
 
+console.log(`\nsrcJsOutput (output of ./src-js/*):`);
 console.log(srcJsOutput);
